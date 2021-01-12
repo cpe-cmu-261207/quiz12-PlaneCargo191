b72ca37e5d102bc2028484edd7cc90b9ea7fbe89
PlaneCargo191 <t.james_17@hotmail.com>
2021-01-12 13:59:58 +0700

    New

--- app_template.js
+++ app_template.js
@@ -1,1 +1,9 @@
-//your code here!
+const body = document.body
+document.addEventListener('DOMContentLoaded', function(){
+    function update() {
+        const inputN = document.querySelector("#button-compute").value
+        let result = 620610790 + inputN;
+        document.querySelector('#compute-result').innerHTML = result
+    }
+    document.querySelector('input').onkeyup = update
+})
